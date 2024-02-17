4337339a168db7ede0df05aac3268fc009a341d2
Marcos De Vargas <2020-0288@uteco.edu.do>
2024-02-17 13:47:45 -0400

    This the version 2.0

---
+++ Data/Services/IRentalInvoiceServices.cs
@@ -0,0 +1,13 @@
+﻿using RentCar.Data.Request;
+using RentCar.Data.Response;
+
+namespace RentCar.Data.Services
+{
+    public interface IRentalInvoiceServices
+    {
+        Task<Result<List<RentalInvoiceResponse>>> Consultar();
+        Task<Result> Crear(RentalInvoiceRequest request);
+        Task<Result> Eliminar(RentalInvoiceRequest request);
+        Task<Result> Modificar(RentalInvoiceRequest request);
+    }
+}
---
+++ Data/Services/RentalInvoiceServices.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentCar.Data.Context;
+using RentCar.Data.Models;
+using RentCar.Data.Request;
+using RentCar.Data.Response;
+
+namespace RentCar.Data.Services
+{
+    public class RentalInvoiceServices : IRentalInvoiceServices
+    {
+        private readonly IRentCarDbContext dbContext;
+
+        public RentalInvoiceServices(IRentCarDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Result> Crear(RentalInvoiceRequest request)
+        {
+            try
+            {
+                var rentalInvoice = RentalInvoice.Crear(request);
+                dbContext.Invoices.Add(rentalInvoice);
+                await dbContext.SaveChangesAsync();
+                return new Result() { Message = "Ok", Success = true };
+            }
+            catch (Exception E)
+            {
+
+                return new Result() { Message = E.Message, Success = false };
+            }
+        }
+        public async Task<Result> Modificar(RentalInvoiceRequest request)
+        {
+            try
+            {
+                var invoice = await dbContext.Invoices
+                    .FirstOrDefaultAsync(r => r.Id == request.Id);
+                if (invoice == null)
+                    return new Result() { Message = "No se encontro la factura de renta", Success = false };
+
+                if (invoice.Modificar(request))
+                    await dbContext.SaveChangesAsync();
+
+                return new Result() { Message = "Ok", Success = true };
+            }
+            catch (Exception E)
+            {
+
+                return new Result() { Message = E.Message, Success = false };
+            }
+        }
+        public async Task<Result> Eliminar(RentalInvoiceRequest request)
+        {
+            try
+            {
+                var rentalInvoice = await dbContext.Invoices
+                    .FirstOrDefaultAsync(r => r.Id == request.Id);
+                if (rentalInvoice == null)
+                    return new Result() { Message = "No se encontro el usuario", Success = false };
+
+                dbContext.Invoices.Remove(rentalInvoice);
+                await dbContext.SaveChangesAsync();
+                return new Result() { Message = "Ok", Success = true };
+            }
+            catch (Exception E)
+            {
+
+                return new Result() { Message = E.Message, Success = false };
+            }
+        }
+        public async Task<Result<List<RentalInvoiceResponse>>> Consultar()
+        {
+            try
+            {
+                var facturas = await dbContext.Invoices
+                    .AsNoTracking()
+                    .IgnoreAutoIncludes()
+                    .Include(f => f.Customer)
+                    .Include(f => f.Vehicle)
+                    .Select(f => f.ToResponse())
+                    .ToListAsync();
+                return new Result<List<RentalInvoiceResponse>>()
+                {
+                    Data = facturas,
+                    Success = true,
+                    Message = "Ok"
+                };
+            }
+            catch (Exception E)
+            {
+                return new Result<List<RentalInvoiceResponse>>()
+                {
+                    Data = null,
+                    Success = false,
+                    Message = E.Message
+                };
+            }
+        }
+    }
+}
